4314f46f082ab4f993e7cc64ba47060e98eaaf73
Hirohito-lya <83554334+Hirohito-lya@users.noreply.github.com>
2021-07-08 17:32:26 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,21 @@
+import time
+import hashlib as hasher
+
+print('Sha256: Her metni 64 karakterlik şifrelenmiş yazıya dönüştürür -Seçmek için 1- \n')
+print('Md5: Girilen verinin boyutundan bağımsız olarak, 128-bit özet değeri üretir -Seçmek için 2-')
+inp = input('Şifreleme türünü seçin: \n')
+
+if inp == "1":
+    sif = hasher.sha256()
+    tx = input('Şifrelenecek metni giriniz: ')
+    sif.update(tx.encode('utf-8'))
+    hsh = sif.hexdigest()
+    time.sleep(2)
+    print(hsh)
+elif inp == "2":
+    sif = hasher.md5()
+    tx = input('Şifrelenecek metni giriniz: ')
+    sif.update(tx.encode('utf-8'))
+    hsh = sif.hexdigest()
+    time.sleep(2)
+    print(hsh)
